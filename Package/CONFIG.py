93ea97d887d664876d415799c310351ddd5168a3
yuanyu.lin <yuanyu.lin@gmail.com>
2017-04-23 12:50:11 +0800

    MAJOR_NUM=1 MINOR_NUM=3 AUX_NUM=0 rework folder structure.

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -1,10 +1,46 @@
 import ops
 
+def MAIN_ENV(args):
+    pkg_path = args["pkg_path"]
+    output_dir = args["output_path"]
+    base_rootfs_dir = output_dir + "/debian_jessie_armhf"
+    base_rootfs_tarball = pkg_path + "/debian_jessie_armhf.tar.xz"
+    env = ops.setEnv("BASE_ROOTFS_DIR", base_rootfs_dir)
+    ops.exportEnv(env)
+
+    env = ops.setEnv("BASE_ROOTFS_TARBALL", base_rootfs_tarball)
+    ops.exportEnv(env)
+    return False
+
+def MAIN_EXTRACT(args):
+    arch = ops.getEnv("ARCH_ALT")
+    output_dir = args["output_path"]
+    base_rootfs_tarball = ops.getEnv("BASE_ROOTFS_TARBALL")
+    if ops.isExist(base_rootfs_tarball):
+        ops.extractTarXz(base_rootfs_tarball, output_path)
+    else:
+        output_path=ops.pkg_mkdir(output_dir, "debian_jessie_armhf")
+        download_url = 'ftp://ftp.debian.org/debian/'
+        CMD=["qemu-debootstrap", "--arch=" + arch, "jessie", output_path, download_url]
+        res = ops.execCmd(CMD, output_path, False, None)
+    return True
+
+def MAIN_CONFIGURE(args):
+    output_dir = args["output_path"]
+    return False
+
+def MAIN_BUILD(args):
+    output_dir = args["output_path"]
+    return False
+
+def MAIN_INSTALL(args):
+    output_dir = args["output_path"]
+    return False
+
+def MAIN_CLEAN_BUILD(args):
+    output_dir = args["output_path"]
+    return False
+
 def MAIN(args):
-    output_path=ops.pkg_mkdir(args["pkg_path"], "debian_jessie_armhf")
-    qemu_path=ops.pkg_mkdir(output_path, "/usr/bin")
-    print qemu_path
-    ops.copyto("/usr/bin/qemu-arm-static", output_path + "/usr/bin/qemu-arm-static")
-    arch="armhf"
-    CMD=["debootstrap", "--arch=" + arch, "--variant=minbase", "jessie", output_path]
-    res = ops.execCmd(CMD, output_path, False, None)
+    output_dir = args["output_path"]
+
